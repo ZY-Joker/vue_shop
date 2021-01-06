9b425930a148f078ac6fb3c18b6636ed61257789
Neuropathy12 <2585008559@qq.com>
2021-01-06 22:58:29 +0800

    Vue实战-第二次提交--登陆界面实现

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    进入页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Login from '../components/Login.vue'
+import Home from '../components/Home'
 Vue.use(VueRouter)
-
+// 路由规则
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  // 当访问“/” 重定向到“/login”
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
