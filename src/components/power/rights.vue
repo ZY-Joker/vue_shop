af58cfd58aab838546874200e14e2fa7cf53f45e
Neuropathy12 <2585008559@qq.com>
2021-01-16 11:14:59 +0800

    Vue实战-第九次提交--主体部分-权限列表展示完成

---
+++ src/components/power/rights.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+      <!--    面包屑导航区-->
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+        <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+      </el-breadcrumb>
+      <el-card>
+        <el-table
+          :data="rightsList"
+          border
+          stripe
+          style="width: 100%">
+          <el-table-column
+            type="index">
+          </el-table-column>
+          <el-table-column
+            prop="authName"
+            label="权限名称"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="path"
+            label="路径">
+          </el-table-column>
+          <el-table-column
+            prop="level"
+            label="权限等级">
+            <template slot-scope="scope">
+              <el-tag v-if="scope.row.level === '0'">一级权限</el-tag>
+              <el-tag v-else-if="scope.row.level === '1'" type="success">二级权限</el-tag>
+              <el-tag v-else type="warning">三级权限</el-tag>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'rights',
+  data () {
+    return {
+      rightsList: []
+    }
+  },
+  created () {
+    this.getRightsList()
+  },
+  methods: {
+    async getRightsList () {
+      const { data: res } = await this.$http.get('rights/list')
+      if (res.meta.status !== 200) return this.$message.error('获取权限列表失败')
+      else {
+        this.rightsList = res.data
+        this.total = res.data.total
+        this.$message.success('获取权限列表成功')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
